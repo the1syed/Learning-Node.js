702ad1d20b9fb97354fe2c4a2d90b1237c0dc538
the1syed <05syed08@gmail.com>
2025-06-14 19:56:17 +0530

    Stopping server using  process.exit()

---
+++ StoppingServer.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    console.log(req);
+    process.exit(); // Stops event loop
+});
+
+server.listen(PORT, () =>{
+    console.log(`Server started and running at http://localhost:${PORT}`);
+})
